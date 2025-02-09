052b25a6f5fb185db85b1afa2cf62b8e54a05bdf
seveibar <seveibar@gmail.com>
2025-02-08 17:08:34 -0800

    update snapshots

--- tests/test3-svg-url.test.ts
+++ tests/test3-svg-url.test.ts
@@ -14,6 +14,6 @@ export default () => (
   )
 
   expect(url).toMatchInlineSnapshot(
-    `"https://svg.tscircuit.com/?svg_type=pcb&code=H4sIAN2jkmcAAy2NTQ7CIBhE95xiwqpdtVWXhUO4ckuBClF%2BAp%2FRxHh30Xb3JvMyw%2Bwrp0IwdlWPO6HrISQ6BsxLUsXg6Q05wacxBA5n%2FdXRnmSTmlZs9ZVSwQYqatuEG8eaEuXiY%2FPH03jgiCq06jxxVO0u4n38IOtlg%2BG3Ng%2F%2FT8l69gUyPWjAmAAAAA%3D%3D"`,
+    `"https://svg.tscircuit.com/?svg_type=pcb&code=H4sIAA4AqGcAAy2NTQ7CIBhE95xiwqpdtVWXhUO4ckuBClF%2BAp%2FRxHh30Xb3JvMyw%2Bwrp0IwdlWPO6HrISQ6BsxLUsXg6Q05wacxBA5n%2FdXRnmSTmlZs9ZVSwQYqatuEG8eaEuXiY%2FPH03jgiCq06jxxVO0u4n38IOtlg%2BG3Ng%2F%2FT8l69gUyPWjAmAAAAA%3D%3D"`,
   )
 })
